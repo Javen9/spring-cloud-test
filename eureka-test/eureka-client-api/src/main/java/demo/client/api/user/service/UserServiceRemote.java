f6e31e058aec4309fa336eed333c933fa594d92e
Javen91 <673544001@qq.com>
2017-10-26 15:27:41 +0800

    调整注释说明

--- eureka-test/eureka-client-api/src/main/java/demo/client/api/user/service/UserServiceRemote.java
+++ eureka-test/eureka-client-api/src/main/java/demo/client/api/user/service/UserServiceRemote.java
@@ -15,7 +15,10 @@ public interface UserServiceRemote {
     @RequestMapping(value = "createUser")
     int createUser(UserDTO userDTO);
 
-//不支持多个对象传输，暂时没有找到解决方法(无法像dubbo一样可以想本地方法一样定义接口)
+// Feign不支持多个对象情况，Feign是基于http请求方式，一个请求只能有一个body，而对象不指定为@RequestParam，Feign会把它当做一个body
+// 当你把封装对象指定为@RequestParam时，Feign传输时会toString，最后传输的是内存地址，服务端无法正常获取到参数
+// 也就是说Feign只支持传一个封装类对象，有其他需要做特殊处理，比如转成json字符串传输
+// 这点从使用上是与dubbo最大的区别，dubbo没有这个限制，参数可以像本地方法一样随意定义
 //    @RequestMapping(value = "createUser2")
 //    String createUser(Integer id, UserDTO userDTO, UserAddrDTO userAddrDTO);
 
